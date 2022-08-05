7ea9b305fe31865c1b1c572dd29d30a144f59b52
Pulse <shelvanek@mail.ru>
2022-08-05 21:22:29 +0300

    Определили массив

---
+++ itogi/Program.cs
@@ -0,0 +1,11 @@
+﻿int arrSize = Convert.ToInt32(Console.ReadLine());
+string [] arr = new string [arrSize];
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        arr [i] = Console.ReadLine();
+    }
+Console.Write($"[{String.Join("; ", arr)}]");
+
+
+
